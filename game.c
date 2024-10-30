35816f8a97feda0071d3ea6d14cb5a6b778c6ca7
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2024-10-30 16:40:50 +0500

    level 1

--- game.c
+++ game.c
@@ -4,12 +4,15 @@
 #include <time.h>
 #include <conio.h>
 #include <windows.h>
+#include <ctype.h>
 
 #define GRID_SIZE 25
 #define PLAYER 'U'
 #define OBJECT '#'
 #define END_POSITION '$'
 #define NUM_OF_OBJECTS 150
+#define NUM_OF_MONSTERS 24
+#define MONSTER 'M'
 #define ROWS 28
 #define COLS 25
 
@@ -23,11 +26,11 @@ void setCoordinates(int x, int y)
 
 int playerX, playerY,                                 // Players coordinates
     objectX[NUM_OF_OBJECTS], objectY[NUM_OF_OBJECTS], // array of coordinates of all objects in the maze
-    endPointX, endPointY;                             // endpoint/finishpoint coordinates
+    endPointX, endPointY,                             // endpoint/finishpoint coordinates
+    monsterX[NUM_OF_MONSTERS], monstersY[NUM_OF_MONSTERS];
 
 void resetConditions()
-{ // resets the game conditions and make it ready for new game
-    int i;
+{                // resets the game conditions and make it ready for new game
     playerX = 0; // resets the position of player
     playerY = 0;
 
@@ -36,10 +39,15 @@ void resetConditions()
 
     srand(time(NULL)); // ensures that everytime rand() produces random numbers in random order
 
-    for (i = 0; i < NUM_OF_OBJECTS; i++) // looping till NUM_OF_OBJECTS and populating the X and Y coordinate arrays of objects
+    for (int i = 0; i < NUM_OF_OBJECTS; i++) // looping till NUM_OF_OBJECTS and populating the X and Y coordinate arrays of objects
     {
         objectX[i] = rand() % GRID_SIZE;
         objectY[i] = rand() % GRID_SIZE;
+    }
+    for (int i = 0; i < NUM_OF_MONSTERS; i++) // looping till NUM_OF_OBJECTS and populating the X and Y coordinate arrays of objects
+    {
+        monsterX[i] = rand() % GRID_SIZE;
+        monstersY[i] = rand() % GRID_SIZE;
     }
 }
 
@@ -71,9 +79,24 @@ void generateMaze()
                         break;
                     }
                 }
+
+                int isMonster = 0;                        // flag to check if there should be a monster at that position
+                for (int k = 0; k < NUM_OF_MONSTERS; k++) // loops through the number of monsters i-e; 24
+                {
+                    if (j == monsterX[k] && i == monstersY[k]) // checks if the current j and i position of the row and column corresponds to the x and y position of the current index of monter in the array
+                    {
+                        isMonster = 1; // if yes, then the flag is raised as true that means that we have to put a monster there
+                        break;
+                    }
+                }
+
                 if (isObject)
                 {
                     printf("%c ", OBJECT); // printing object if the flag is true
+                }
+                else if (isMonster)
+                {
+                    printf("%c ", MONSTER); // printing monster if the flag is true
                 }
                 else
                 {
@@ -86,16 +109,73 @@ void generateMaze()
     printf("+----------------------------------------------------+");
 }
 
-int main()
+int movePlayer(char direction)
 {
-    printf("git checking");
-    setCoordinates(29, 14);
-    printf("Ayan");
-    Sleep(800);
-    printf(" Mirza");
-    getch();
+    int newX = playerX;
+    int newY = playerY;
+
+    switch (tolower(direction))
+    {
+    case 'w':
+        newY--;
+        break;
+    case 's':
+        newY++;
+        break;
+    case 'a':
+        newX--;
+        break;
+    case 'd':
+        newX++;
+        break;
+    }
+    if (newX >= 0 && newX < GRID_SIZE && newY >= 0 && newY < GRID_SIZE) // cheks if the new X and Y coordinates of player are greater than or equal to zero as they can't be negative and whether they are within the grid
+    {
+        playerX = newX;
+        playerY = newY;
+    }
+
+    generateMaze(); // generate updated maze with new player position
+}
+
+void level1()
+{
     resetConditions();
     generateMaze();
+
+    char input;
+    while (1)
+    {
+        input = getch();
+        if (tolower(input) == 'q')
+        {
+            return;
+        }
+        movePlayer(input);
+
+        if (playerX == endPointX && playerY == endPointY)
+        {
+            printf("Congratulations! You've reached the endpoint.\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    setCoordinates(29, 14);
+    printf("WELCOME LAD THERE ARE THREE LEVELS TO THIS GAME WANNA PLAY [Y/y]: ");
+    char input;
+    scanf("%c", &input);
+    if (tolower(input) == 'y')
+    {
+        level1();
+    }
+    else
+    {
+        setCoordinates(29, 18);
+        printf("THEN BE ON YOUR WAY");
+    }
     // system("\"C:/Users/Asad Mirza/Desktop/Malicious-Maze-Game/setup.bat\"");
     return 0;
 }
