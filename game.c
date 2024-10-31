34e3e99fe4958fcd14fcd5b7f409d53736c2fd04
syedazan11 <syeazz11@gmail.com>
2024-10-31 09:57:31 +0000

    lvl 2

--- game.c
+++ game.c
@@ -29,6 +29,60 @@ int playerX, playerY,                                 // Players coordinates
     endPointX, endPointY,                             // endpoint/finishpoint coordinates
     monsterX[NUM_OF_MONSTERS], monstersY[NUM_OF_MONSTERS];
 
+void introduction()
+{
+    system("cls");
+    // PlaySoundA(TEXT("C:\\Users\\kk\\Desktop\\project\\intros.wav"), NULL, SND_FILENAME | SND_ASYNC);
+
+    int n;
+    printf("\n\n\n\t\tWelcome, Captain Leo Mason...\n");
+    printf("\t\tEarth is in grave danger, and humanity's last hope rests on you.\n\n");
+
+    printf("\t\tI am the Commander of Mission Control.\n\t\tWe have entrusted you with a critical task that only you can complete.\n\n");
+    printf("\t\t(1. Understood, Commander. What’s the situation? / 2. Just get to the point, Commander!) (1/2): ");
+    scanf("%d", &n);
+
+    if (n == 2)
+        printf("\n\t\tFair enough, Captain. Let's skip the formalities...\n");
+    else
+        printf("\n\t\tGlad to see you’re prepared, Captain.\n");
+
+    printf("\n\t\tEarth is suffering from a massive environmental crisis.\n\t\tThe scientists believe that the Solum Sphere, an artifact on the planet Krypton, is our last hope.\n");
+    getch();
+
+    printf("\n\t\tThe Solum Sphere has the power to restore balance to our ecosystem.\n\t\tYour mission is simple, but dangerous: retrieve the Sphere and return.\n");
+    getch();
+
+    printf("\n\t\tKrypton is not without its perils...\n\t\tYou’ll face three major trials to reach the artifact.\n");
+    printf("\n\t\t(1. Trials? Bring them on! / 2. Do I have a choice?) (1/2): ");
+    scanf("%d", &n);
+
+    if (n == 2)
+        printf("\n\t\tNot really, Captain. Humanity is counting on you.\n");
+    else
+        printf("\n\t\tThat’s the spirit, Captain!\n");
+
+    printf("\n\t\tFirst, you must navigate the Maze of Shadows, a labyrinth designed to confuse even the most skilled travelers.\n");
+    printf("\t\tIf you make it to the end, a mysterious figure may grant you a gift: a Time Watch with three lives.\n");
+    getch();
+
+    printf("\n\t\tThe Time Watch will grant you a second chance if things go south. But be careful—once all lives are spent, the mission ends.\n\n");
+    printf("\t\t(1. Understood. I’ll be careful. / 2. So, no room for mistakes, huh?) (1/2): ");
+    scanf("%d", &n);
+
+    printf("\n\t\tIn the second level, you’ll face a monstrous creature. Your Time Watch may be the only thing standing between you and certain doom.\n");
+    getch();
+
+    printf("\n\t\tFinally, the last challenge awaits in the Heart of Krypton—a maze at the core of the planet where the Solum Sphere resides.\n\t\tRetrieve it, and Earth will be saved.\n\n");
+
+    printf("\t\tAre you ready, Captain Leo Mason?\n\t\t(1. For Earth! / 2. Here goes nothing...) (1/2): ");
+    scanf("%d", &n);
+
+    printf("\n\t\tGood luck, Captain. Humanity’s fate is in your hands. We'll see you when you return.\n");
+    printf("\n\t\tPRESS ANY KEY TO BEGIN YOUR MISSION...");
+    getch();
+}
+
 void resetConditions()
 {                // resets the game conditions and make it ready for new game
     playerX = 0; // resets the position of player
@@ -51,7 +105,7 @@ void resetConditions()
     }
 }
 
-void generateMaze()
+void generateMaze(int includeMonsters)
 {
     system("cls");
     printf("+----------------------------------------------------+\n");
@@ -80,13 +134,16 @@ void generateMaze()
                     }
                 }
 
-                int isMonster = 0;                        // flag to check if there should be a monster at that position
-                for (int k = 0; k < NUM_OF_MONSTERS; k++) // loops through the number of monsters i-e; 24
+                int isMonster = 0; // flag to check if there should be a monster at that position
+                if (includeMonsters)
                 {
-                    if (j == monsterX[k] && i == monstersY[k]) // checks if the current j and i position of the row and column corresponds to the x and y position of the current index of monter in the array
+                    for (int k = 0; k < NUM_OF_MONSTERS; k++) // loops through the number of monsters i-e; 24
                     {
-                        isMonster = 1; // if yes, then the flag is raised as true that means that we have to put a monster there
-                        break;
+                        if (j == monsterX[k] && i == monstersY[k]) // checks if the current j and i position of the row and column corresponds to the x and y position of the current index of monter in the array
+                        {
+                            isMonster = 1; // if yes, then the flag is raised as true that means that we have to put a monster there
+                            break;
+                        }
                     }
                 }
 
@@ -109,7 +166,7 @@ void generateMaze()
     printf("+----------------------------------------------------+");
 }
 
-int movePlayer(char direction)
+int movePlayer(char direction, int includeMonsters)
 {
     int newX = playerX;
     int newY = playerY;
@@ -142,12 +199,15 @@ int movePlayer(char direction)
             }
         }
 
-        for (int i = 0; i < NUM_OF_MONSTERS; i++)
+        if (includeMonsters)
         {
-            if (newX == monsterX[i] && newY == monstersY[i])
+            for (int i = 0; i < NUM_OF_MONSTERS; i++)
             {
-                isAMonster = 1;
-                return isAMonster;
+                if (newX == monsterX[i] && newY == monstersY[i])
+                {
+                    isAMonster = 1;
+                    return isAMonster;
+                }
             }
         }
 
@@ -158,14 +218,14 @@ int movePlayer(char direction)
         }
     }
 
-    generateMaze(); // generate updated maze with new player position
+    generateMaze(includeMonsters); // generate updated maze with new player position
     return 0;
 }
 
 void level1()
 {
     resetConditions();
-    generateMaze();
+    generateMaze(0);
 
     char input;
     while (1)
@@ -175,11 +235,35 @@ void level1()
         {
             return;
         }
-        int checkMonster = movePlayer(input);
+        int checkMonster = movePlayer(input, 0);
+
+        if (playerX == endPointX && playerY == endPointY)
+        {
+            printf("Congratulations! You've reached the endpoint.\n");
+            return ;
+        }
+    }
+}
+void level2()
+{
+    resetConditions();
+    generateMaze(1);
+
+    char input;
+    while (1)
+    {
+        input = getch();
+        if (tolower(input) == 'q')
+        {
+            return;
+        }
+        int checkMonster = movePlayer(input, 1);
 
         if (checkMonster)
         {
             system("cls");
+            setCoordinates(29, 18);
+            printf("You have been eaten by the monster");
             break;
         }
 
@@ -189,25 +273,18 @@ void level1()
             break;
         }
     }
-    setCoordinates(29, 18);
-    printf("You have been eaten by the monster");
 }
 
 int main()
 {
     setCoordinates(29, 14);
-    printf("WELCOME LAD THERE ARE THREE LEVELS TO THIS GAME WANNA PLAY [Y/y]: ");
-    char input;
-    scanf("%c", &input);
-    if (tolower(input) == 'y')
-    {
-        level1();
-    }
-    else
-    {
-        setCoordinates(29, 18);
-        printf("THEN BE ON YOUR WAY");
-    }
+    introduction();
+    system("cls");
+    Sleep(500);
+    level1();
+    system("cls");
+    Sleep(500);
+    level2();
     // system("\"C:/Users/Asad Mirza/Desktop/Malicious-Maze-Game/setup.bat\"");
     return 0;
 }
